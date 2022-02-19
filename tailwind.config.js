a3f6710b57161d66201e260866e45273eda72dd3
Hiroshi Fujita <fujita-h@users.noreply.github.com>
2022-02-19 16:13:27 +0900

    set Noto Sans JP as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       fontFamily: {
         sans: [
           'YakuHanJPs',
+          'Noto Sans JP',
           '-apple-system',
           'BlinkMacSystemFont',
           'Segoe UI',
